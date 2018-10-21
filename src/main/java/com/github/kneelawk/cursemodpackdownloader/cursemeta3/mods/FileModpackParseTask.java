4b2c63c202ff89585ea7639dfb00a83340d5cd82
Kneelawk <jedidiah.pommert@gmail.com>
2018-10-21 00:44:06 -0700

    FileModpackParseTask updates project property

--- src/main/java/com/github/kneelawk/cursemodpackdownloader/cursemeta3/mods/FileModpackParseTask.java
+++ src/main/java/com/github/kneelawk/cursemodpackdownloader/cursemeta3/mods/FileModpackParseTask.java
@@ -9,6 +9,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.json.FileDataJson;
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.json.FileId;
+import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.json.FileJson;
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.net.Downloader;
 import com.google.gson.Gson;
 
@@ -38,9 +39,15 @@ public class FileModpackParseTask extends ModpackParseTask {
 		if (id != null) {
 			// is this modpack file an xml?
 			updateMessage("Loaded xml modpack, downloading zip...");
+			FileDataJson data = AddonUtils.getAddonFile(client, gson, id);
+
+			FileJson file =
+					new FileJson.Builder(id.getProjectID(), id.getFileID())
+							.setFileData(data).build();
+			updateProject(file);
+
 			Path modpackPath = Files.createTempFile("modpack", ".zip");
 			modpackPath.toFile().deleteOnExit();
-			FileDataJson data = AddonUtils.getAddonFile(client, gson, id);
 			Downloader downloader =
 					new Downloader(client, data.getDownloadUrl(), modpackPath);
 			downloader.progressProperty().addListener((o, oldVal,
