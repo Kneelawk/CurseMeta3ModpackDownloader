35bf127f5ee4406084c0d425fb9499cd25c40ec2
Kneelawk <jedidiah.pommert@gmail.com>
2018-10-22 19:59:07 -0700

    Use PoolingHttpClientConnectionManager

--- src/main/java/com/github/kneelawk/cursemodpackdownloader/cursemeta3/CurseMeta3ModpackDownloader.java
+++ src/main/java/com/github/kneelawk/cursemodpackdownloader/cursemeta3/CurseMeta3ModpackDownloader.java
@@ -17,7 +17,7 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.FileModpackParseTask;
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.Modpack;
@@ -50,7 +50,7 @@ public class CurseMeta3ModpackDownloader extends Application {
 				.register("https", connectionSocketFactory)
 				.register("http", PlainConnectionSocketFactory.INSTANCE)
 				.build();
-		HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(
+		HttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(
 				registry);
 
 		CloseableHttpClient client = HttpClients.custom()
