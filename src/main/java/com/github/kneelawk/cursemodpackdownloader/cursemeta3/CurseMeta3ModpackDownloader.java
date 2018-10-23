28dbdaf51d1bd2cff5c3e1453c1fffd5783edc59
Kneelawk <jedidiah.pommert@gmail.com>
2018-10-22 19:48:19 -0700

    Fix SSL errors

--- src/main/java/com/github/kneelawk/cursemodpackdownloader/cursemeta3/CurseMeta3ModpackDownloader.java
+++ src/main/java/com/github/kneelawk/cursemodpackdownloader/cursemeta3/CurseMeta3ModpackDownloader.java
@@ -4,11 +4,20 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.net.ssl.SSLContext;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.FileModpackParseTask;
 import com.github.kneelawk.cursemodpackdownloader.cursemeta3.mods.Modpack;
@@ -28,13 +37,32 @@ public class CurseMeta3ModpackDownloader extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Gson gson = new Gson();
+		/*
+		 * Fix for weird ssl errors provided by:
+		 * https://stackoverflow.com/a/52000542
+		 */
+		SSLContext sslContext = SSLContext.getDefault();
+		SSLConnectionSocketFactory connectionSocketFactory = new SSLConnectionSocketFactory(
+				sslContext, new String[] { "TLSv1.2" }, null,
+				NoopHostnameVerifier.INSTANCE);
+		Registry<ConnectionSocketFactory> registry = RegistryBuilder
+				.<ConnectionSocketFactory>create()
+				.register("https", connectionSocketFactory)
+				.register("http", PlainConnectionSocketFactory.INSTANCE)
+				.build();
+		HttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(
+				registry);
+
 		CloseableHttpClient client = HttpClients.custom()
-				.setRedirectStrategy(new RedirectUriSanitizer()).build();
+				.setRedirectStrategy(new RedirectUriSanitizer())
+				.setSSLSocketFactory(connectionSocketFactory)
+				.setConnectionManager(connectionManager).build();
+
+		Gson gson = new Gson();
 		DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
-		ExecutorService executor =
-				Executors.newSingleThreadExecutor(runnable -> {
+		ExecutorService executor = Executors
+				.newSingleThreadExecutor(runnable -> {
 					Thread t = new Thread(runnable);
 					t.setDaemon(true);
 					return t;
